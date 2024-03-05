b5a2ecdc516c8ed1df7c8d58c1df81065656ec0e
Dspil <dennisspiliopoylos@gmail.com>
2024-03-05 02:14:00 +0100

    random key

--- test_chaos.py
+++ test_chaos.py
@@ -2,24 +2,7 @@ from random import randint as rand
 from chaos_encryption import encrypt, decrypt
 
 # 128bit key (randomly picked) split into 16 bytes
-K = [
-    0x3F,
-    0xDF,
-    0x1D,
-    0xD5,
-    0xEA,
-    0xA9,
-    0x16,
-    0x1D,
-    0x0D,
-    0x16,
-    0x03,
-    0x1B,
-    0xB3,
-    0x8E,
-    0xAC,
-    0xF7,
-]
+K = [rand(0, 255) for i in range(16)]
 
 img = [
     [rand(0, 255) for i in range(256)] for j in range(256)
